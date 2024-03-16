34a11e1dd7184e3ad19ce719086d6337b0466067
FlorianPetronin <florian.petronin4@etu.univ-lorraine.fr>
2024-03-16 16:30:39 +0100

    Setting CORS security

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 
+
+
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/book_image/<ean>', methods=['GET'])
 def get_images(ean):
