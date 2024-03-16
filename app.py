3652315b35e3f02b76e7fc937fdb991de343fa0f
FlorianPetronin <florian.petronin4@etu.univ-lorraine.fr>
2024-03-16 15:46:00 +0100

    Update port

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def get_images(ean):
     if os.path.isfile('static/images/'+ ean + '.jpg'):
         return jsonify({
             'ean' : ean,
-            'url' : "http://51.75.21.24:5000/static/images/" + ean + '.jpg'
+            'url' : "http://51.75.21.24:88/static/images/" + ean + '.jpg'
         }), 200
     else:
         return jsonify({
             'error' : 'This book has no cover yet',
-            'url' : "http://51.75.21.24:5000/static/images/no_cover.jpg"
+            'url' : "http://51.75.21.24:88/static/images/no_cover.jpg"
         }), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=88)
